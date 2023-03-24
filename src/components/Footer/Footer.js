83f3bab020ebd4b37f411dd2ff1a819d253c6c85
alifmutaram <alifmutaram22@gmail.com>
2023-03-24 20:40:28 +0700

    menambahkan feature (footer)

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,46 @@
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <div className="addres">
+          <p>Jalan Suroyo No. 161 Mayangan Kota Probolonggo 672000</p>
+          <p>binarcarrental@gmail.com</p>
+          <p>081-233-334-808</p>
+        </div>
+        <div className="d-flex flex-column align-items-start nav-foot">
+          <a href="">Our services</a>
+          <a href="">Why Us</a>
+          <a href="">Testimonial</a>
+          <a href="">FAQ</a>
+        </div>
+        <div className="container-footer"></div>
+        <div className="sosmed">
+          <p>Connect with us</p>
+          <div className="d-flex flex-row align-items-start list-sosmed">
+            <a href="">
+              <img src="/assets/icon_facebook.png" alt="facebook" />
+            </a>
+            <a href="">
+              <img src="/assets/icon_instagram.png" alt="instagram" />
+            </a>
+            <a href="">
+              <img src="/assets/icon_twitter.png" alt="twitter" />
+            </a>
+            <a href="">
+              <img src="/assets/icon_mail.png" alt="mail" />
+            </a>
+            <a href="">
+              <img src="/assets/icon_twitch.png" alt="twitch" />
+            </a>
+          </div>
+        </div>
+        <div className="copyright">
+          <p>Copyright Binar 2022</p>
+          <div className="rectacle-foot"></div>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
