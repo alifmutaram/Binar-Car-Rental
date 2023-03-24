2f5af5f8089352cf63c20c884e5fff750a274137
alifmutaram <alifmutaram22@gmail.com>
2023-03-25 06:04:15 +0700

    menambahkan feat(FAQ)

---
+++ src/components/FAQ/Faq.js
@@ -0,0 +1,67 @@
+const FAQ = () => {
+  return (
+    <>
+      <div className="faq">
+        <div className="text-faq">
+          <p className="text-faq-1">Frequently Asked Question</p>
+          <p className="text-faq-2">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit
+          </p>
+        </div>
+        <div className="list-faq">
+          <button
+            class="btn dropdown-toggle btn-faq"
+            type="button"
+            id="dropdownMenu2"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            <p>Apa saja syarat yang dibutuhkan?</p>
+          </button>
+          <button
+            class="btn dropdown-toggle btn-faq"
+            type="button"
+            id="dropdownMenu2"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            <p>Berapa hari minimal sewa mobil lepas kunci?</p>
+          </button>
+          <button
+            class="btn dropdown-toggle btn-faq"
+            type="button"
+            id="dropdownMenu2"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            <p>Berapa hari sebelumnya sabaiknya booking sewa mobil?</p>
+          </button>
+          <button
+            class="btn dropdown-toggle btn-faq"
+            type="button"
+            id="dropdownMenu2"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            <p>Apakah Ada biaya antar-jemput?</p>
+          </button>
+          <button
+            class="btn dropdown-toggle btn-faq"
+            type="button"
+            id="dropdownMenu2"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            <p>Bagaimana jika terjadi kecelakaan</p>
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+export default FAQ;
