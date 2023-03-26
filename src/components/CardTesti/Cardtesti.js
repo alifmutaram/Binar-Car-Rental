8f4a39b044681434092e93614c8fb383c0b6634b
alifmutaram <alifmutaram22@gmail.com>
2023-03-26 17:06:20 +0700

    menambahkan component Card Untuk feat(testimonial)

---
+++ src/components/CardTesti/Cardtesti.js
@@ -0,0 +1,16 @@
+const CardTesti = ({ testiImg, testiText, nameTesti }) => {
+  return (
+    <>
+      <div className="card-testi d-flex flex-row justify-content-around align-items-center">
+        <img src={testiImg} />
+        <div className="box-text d-flex flex-column align-items-start">
+          <img src="/assets/Rate.png" className="rate" />
+          <p>{testiText}</p>
+          <span>{nameTesti}</span>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CardTesti;
