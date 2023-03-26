28b31d24cdaa51159b3122359eb1df55e48b3e84
alifmutaram <alifmutaram22@gmail.com>
2023-03-26 17:19:45 +0700

    menambahkan id pada navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -14,22 +14,22 @@ const Navbar = () => {
             <span>bcr</span>
           </div>
           <li className="nav-item">
-            <a className="nav-link color" href="#">
+            <a className="nav-link color" href="#our-service">
               Our Services
             </a>
           </li>
           <li className="nav-item">
-            <a className="nav-link color" href="#">
+            <a className="nav-link color" href="#why-us">
               Why Us
             </a>
           </li>
           <li className="nav-item">
-            <a className="nav-link color" href="#">
+            <a className="nav-link color" href="#testimonial">
               Testimonial
             </a>
           </li>
           <li className="nav-item">
-            <a className="nav-link color" href="#">
+            <a className="nav-link color" href="#faq">
               FAQ
             </a>
           </li>
