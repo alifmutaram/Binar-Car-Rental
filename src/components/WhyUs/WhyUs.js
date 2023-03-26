78de8e6185b471e295e892cea807697d7cb92c97
alifmutaram <alifmutaram22@gmail.com>
2023-03-26 17:03:04 +0700

    menambahkan feature (Why Us)

---
+++ src/components/WhyUs/WhyUs.js
@@ -0,0 +1,20 @@
+const WhyUs = ({ children }) => {
+  return (
+    <>
+      <div
+        className="d-flex flex-column justify-content-between why-us"
+        id="why-us"
+      >
+        <div className="text-decs-why">
+          <p>why us</p>
+        </div>
+        <div className="text-decs-why-2">
+          <p>Mengapa harus pilih Binar Car Rental?</p>
+        </div>
+        <div className="list-items">{children}</div>
+      </div>
+    </>
+  );
+};
+
+export default WhyUs;
