2e5d0263712af90d97a20ad92df27c607f83bab2
alifmutaram <alifmutaram22@gmail.com>
2023-03-26 17:08:21 +0700

    menambahkan folder pages untuk Routing

---
+++ src/pages/Home/HomePage.js
@@ -0,0 +1,73 @@
+import Navbar from "../../components/Navbar/Navbar";
+import "../../components/Navbar/navbar.css";
+import HeroSection from "../../components/HeroSection/HeroSection";
+import "../../components/HeroSection/herosection.css";
+import OurService from "../../components/OurService/OurService";
+import "../../components/OurService/ourservice.css";
+import WhyUs from "../../components/WhyUs/WhyUs";
+import "../../components/WhyUs/whyus.css";
+import Testimonial from "../../components/Testimonial/Testimonial";
+import "../../components/Testimonial/testimonial.css";
+import CtaBanner from "../../components/CTABanner/CtaBanner";
+import "../../components/CTABanner/ctabanner.css";
+import FAQ from "../../components/FAQ/Faq";
+import "../../components/FAQ/faq.css";
+import Footer from "../../components/Footer/Footer";
+import "../../components/Footer/footer.css";
+import Card from "../../components/CardWhy/CardWhy";
+import CardTesti from "../../components/CardTesti/Cardtesti";
+
+const HomePage = () => {
+  return (
+    <div>
+      <HeroSection />
+      <Navbar />
+      <OurService />
+      <CtaBanner />
+      <WhyUs>
+        <Card
+          cardImg="/assets/icon_complete.png"
+          cardTitle="Mobil Lengkap"
+          cardText="Tersedia banyak pilihan mobil, kondisi masih baru, bersih dan terawat"
+        />
+        <Card
+          cardImg="/assets/icon_price.png"
+          cardTitle="Harga Murah"
+          cardText="Harga murah dan bersaing, bisa bandingkan harga kami dengan rental mobil lain"
+        />
+        <Card
+          cardImg="/assets/icon_24hrs.png"
+          cardTitle="Layanan 24 Jam"
+          cardText="Siap melayani kebutuhan Anda selama 24 jam nonstop. Kami juga tersedia di akhir minggu"
+        />
+        <Card
+          cardImg="/assets/icon_professional.png"
+          cardTitle="Sopir Profesional"
+          cardText="Sopir yang profesional, berpengalaman, jujur, ramah dan selalu tepat waktu"
+        />
+      </WhyUs>
+      <Testimonial>
+        <CardTesti
+          testiImg="/assets/img_testi-3.png"
+          testiText="“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod”"
+          nameTesti="John Dee 32, Bromo"
+        />
+        <CardTesti
+          testiImg="/assets/img_testi-2.png"
+          testiText="“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod”"
+          nameTesti="John Dee 32, Bromo"
+        />
+        <CardTesti
+          testiImg="/assets/img_testi-3.png"
+          testiText="“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod”"
+          nameTesti="John Dee 32, Bromo"
+        />
+      </Testimonial>
+
+      <FAQ />
+      <Footer />
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ src/pages/SearchCar/CariMobil.js
@@ -0,0 +1,18 @@
+import Navbar from "../../components/Navbar/Navbar";
+import "../../components/Navbar/navbar.css";
+import HeroSection from "../../components/HeroSection/HeroSection";
+import "../../components/HeroSection/herosection.css";
+import Footer from "../../components/Footer/Footer";
+import "../../components/Footer/footer.css";
+
+const CariMobil = () => {
+  return (
+    <>
+      <HeroSection />
+      <Navbar />
+      <Footer />
+    </>
+  );
+};
+
+export default CariMobil;
