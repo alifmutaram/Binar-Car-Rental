19e51a3faf392bcea6282d22406abb8405c84ce8
alifmutaram <alifmutaram22@gmail.com>
2023-04-01 21:54:50 +0700

    membuat dan menambahkan file constants

---
+++ src/constants.js
@@ -0,0 +1,48 @@
+export const catSelect = [
+  {
+    id: 1,
+    text: "2 - 4 Orang",
+    value: "small",
+  },
+  {
+    id: 2,
+    text: "4 - 6 Orang",
+    value: "medium",
+  },
+  {
+    id: 3,
+    text: "6 - 8 Orang",
+    value: "large",
+  },
+];
+
+export const hargaSelect = [
+  {
+    id: 4,
+    text: 400000,
+    value: 400000,
+  },
+  {
+    id: 5,
+    text: 500000,
+    value: 500000,
+  },
+  {
+    id: 7,
+    text: 600000,
+    value: 600000,
+  },
+];
+
+export const statusSelect = [
+  {
+    id: 8,
+    text: "DISEWA",
+    value: true,
+  },
+  {
+    id: 9,
+    text: "KOSONG",
+    value: false,
+  },
+];
